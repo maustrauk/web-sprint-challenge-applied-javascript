0e32a85b449866949fb94e837239222eba75a8de
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-30 12:05:49 -0400

    Task_2 Step_3

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ Your finished project must include all of the following requirements:
 
 - [*] Following the instructions in the `components/Header.js` file, create the Header.
 
-- [ ] Following the instructions in the `components/Tabs.js` file, create the Tabs.
+- [*] Following the instructions in the `components/Tabs.js` file, create the Tabs.
 
 - [ ] Following the instructions in the `components/Cards.js` file, create the Cards.
 
--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,50 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios
+.get("https://lambda-times-api.herokuapp.com/articles")
+.then( data => {
+    const articles = data.data.articles;
+    for (const value of Object.entries(articles)) {
+        value[1].forEach(element => {
+            document.querySelector(".cards-container").appendChild(singleCard(element));
+        });
+    }
+})
+.catch(err => {
+    console.log("Error: ",err);
+})
+
+function singleCard (artObj) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    img.setAttribute("src", artObj.authorPhoto);
+
+    headline.textContent = artObj.headline;
+    name.textContent = `By: ${artObj.authorName}`;
+
+    card.appendChild(headline);
+    card.appendChild(author);
+
+    author.appendChild(imgContainer);
+    author.appendChild(name);
+
+    imgContainer.appendChild(img);
+
+    card.addEventListener('click', () => {
+        console.log("Headline: ", headline.textContent);
+    })
+
+    return card;
+} 
